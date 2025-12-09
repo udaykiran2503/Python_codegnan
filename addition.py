b15f38c3eb21f304df6657c30f97cce3dfc5ba6e
udaykiran2503 <138219919+udaykiran2503@users.noreply.github.com>
2025-12-09 16:32:11 +0530

    Delete addition.py

--- addition.py
+++
@@ -1,7 +0,0 @@
-def add(a, b):
-    return a + b
-
-if __name__ == "__main__":
-    a = float(input("Enter first number: "))
-    b = float(input("Enter second number: "))
-    print("Result:", add(a, b))
