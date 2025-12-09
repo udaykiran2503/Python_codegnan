05238f8c5c1b4cd927475b9b5c4882b13e5ff630
udaykiran2503 <138219919+udaykiran2503@users.noreply.github.com>
2025-12-09 16:55:30 +0530

    Delete Bank app with Database/final_bank_app directory

--- Bank app with Database/final_bank_app/balance.py
+++
@@ -1,9 +0,0 @@
-
-import db_utils
-
-def balance(acc):
-    user=db_utils.get_user(acc)
-    if not user: return "User not found"
-    name,email,amt,acc=user
-    db_utils.singleEmailsender(email,"Balance Check",f"Hello {name}, your balance is {amt}.")
-    return f"Balance: {amt}"
--- Bank app with Database/final_bank_app/login.py
+++
@@ -1,8 +0,0 @@
-
-import mysql.connector as SQLC
-
-def login(acc,pwd):
-    db=SQLC.connect(host="localhost",user="root",password="root",database="BANK1")
-    c=db.cursor()
-    c.execute("SELECT 1 FROM ACCOUNTS WHERE ACCOUNT=%s AND PASSWORD=%s",(acc,pwd))
-    return c.fetchone() is not None
